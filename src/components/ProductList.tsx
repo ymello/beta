dbd750086f4e8d842146ec939e38dbd75cad4af1
Yuri Mello <contato@yurimello.com>
2024-03-06 17:48:06 -0300

    feat: create product list component

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Card } from "./Card";
+
+interface ProductListProps {
+  products: any[];
+}
+
+export function ProductList({ products }: ProductListProps) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10">
+      {products.map((product: any) => (
+        <Card
+          key={product.id}
+          title={product.title}
+          description={product.description}
+          image={product.thumbnail}
+          rating={product.rating}
+          tag={product.category}
+        />
+      ))}
+    </div>
+  );
+}
