02962678863ee51bda5fa9f9d8b87a0fd2e15f01
Yuri Mello <contato@yurimello.com>
2024-03-06 14:07:09 -0300

    feat: login and route protection

---
+++ src/app/products/layout.tsx
@@ -0,0 +1,28 @@
+"use client";
+import NextAuthSessionProvider from "@/providers/sessionProvider";
+import { QueryClientProviderWrapper } from "@/utils/reactQueryConfig";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { data: session, status } = useSession();
+
+  if (status !== "authenticated") {
+    console.log("Não está autenticado");
+    redirect("/");
+  }
+
+  return (
+    <html lang="pt-BR">
+      <body>
+        <QueryClientProviderWrapper>
+          <NextAuthSessionProvider>{children}</NextAuthSessionProvider>
+        </QueryClientProviderWrapper>
+      </body>
+    </html>
+  );
+}
